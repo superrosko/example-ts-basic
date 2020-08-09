2163fd42ceb38dad965c7816963626de09fe7db5
Dmitriy Bespalov <dbespalov.rosko@gmail.com>
2020-08-09 11:26:14 +0300

    feat(examples): add example-web

---
+++ examples/example-web/log.js
@@ -0,0 +1,7 @@
+window.logConsole.log('First message');
+window.logConsole.log('Second message');
+
+const textarea = document.getElementById('textarea');
+window.logTextArea.setTextAreaElement(textarea);
+window.logTextArea.log('First message');
+window.logTextArea.log('Second message');
